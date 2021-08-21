e02f9ad3a6d338ce76fe7d8ca5fe99db477df3a5
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2021-08-21 18:37:47 -0300

    preparando a base do projeto

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import faker from "faker";
+import { StyleSheet, View } from "react-native";
 
-faker.seed(10);
-const DATA = [...Array(30).keys()].map((_, idx) => {
-  return {
-    key: faker.datatype.uuid(),
-    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
-      "women",
-      "men",
-    ])}/${faker.datatype.number(60)}.jpg`,
-    name: faker.name.findName(),
-    ocupation: faker.name.jobTitle(),
-    email: faker.internet.email(),
-  };
-});
-
-const SPACING = 20;
-const AVATAR_SIZE = 70;
+import { AnimateList } from "./src/AnimateList";
+import { BackgroundImage } from "./src/BackgroundImage";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" hidden />
+      <BackgroundImage />
+      <AnimateList />
     </View>
   );
 }
@@ -31,6 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#eee",
   },
 });
